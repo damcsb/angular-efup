4838a85c7c8a0d0d3d975e942ac442636fa4f74c
damcsb <48583917+damcsb@users.noreply.github.com>
2020-05-05 14:00:09 +0200

    implements firebase and authentication serivce

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { AngularFireModule } from '@angular/fire';
+//
+//
 import { environment } from '../environments/environment';
+//
+//
+import { AngularFireModule } from '@angular/fire';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
--- src/app/service/authentication.service.ts
+++ src/app/service/authentication.service.ts
@@ -1,9 +1,53 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class AuthenticationService {
+@Injectable()
+export class AuthService {
+  //
+  //Builder
+  //
+  constructor(public fbAuth: AngularFireAuth) {}
+  //
+  //Methods
+  //
+  async login(email: string, password: string): Promise<any> {
+    try {
+      const result = await this.fbAuth.signInWithEmailAndPassword(
+        email,
+        password
+      );
+      return result;
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-  constructor() { }
+  async register(email: string, password: string): Promise<any> {
+    try {
+      const result = await this.fbAuth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+      return result;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async resetPassword(email: string): Promise<void> {
+    try {
+      return this.fbAuth.sendPasswordResetEmail(email);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async logout(): Promise<void> {
+    try {
+      await this.fbAuth.signOut();
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
